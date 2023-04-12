0da8cc73b599d6a590ec1c331aa86999648aeac7
Matt Hammond <mjhammond94@gmail.com>
2023-04-12 15:14:39 +0100

    present spec updated

--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -1,4 +1,11 @@
 require 'present'
 
 describe Present do 
+  context 'When no contents are passed' do 
+    it "fails" do 
+      present = Present.new
+      present.wrap("toy")
+      expect { present.wrap("toy") }.to raise_error "A contents has already been wrapped."
+    end
+  end
 end
